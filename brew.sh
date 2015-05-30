a1565d5219d94527702171a3b037571a5a1727ae
Brian Ramos <bramos@twitter.com>
2015-05-30 00:32:40 -0700

    more stuff

---
+++ brew.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Make sure we're using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew install bash-completion
+
+# Install more recent versions of some OS X tools.
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/screen
+
+brew install git
+
+
+# Remove outdated versions from the cellar.
+brew cleanup
